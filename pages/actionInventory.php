1d36d43e749a4ef0fb8fbf7feec488d79f3f7338
Ron-De-Guzman <ronrussel002@gmail.com>
2024-10-02 22:19:39 +0800

    actionInventory
    actionInventory

---
+++ pages/actionInventory.php
@@ -0,0 +1,131 @@
+<?php include '../assets/template/navigation.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction Table</title>
+    <link rel="stylesheet" href="../assets/design/actionInventory.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Inventory Actions</h2>
+        <div class="transaction">
+            <table class="transaction-table">
+                <thead>
+                    <tr>
+                        <th>Action ID</th>
+                        <th>Action Type</th>
+                        <th>Item</th>
+                        <th>Quantity</th>
+                        <th>Action Date</th>
+                        <th>Performed By</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>2</td>
+                        <td>Delete Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>Ron</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Delete Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>David</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Add Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>Ron</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Delete Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>David</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Delete Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>David</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Add Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>David</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Delete Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>David</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Add Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>David</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Add Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>David</td>
+                    </tr>
+
+                    <tr>
+                        <td>2</td>
+                        <td>Add Ingredients</td>
+                        <td>Spanish Latte</td>
+                        <td>5</td>
+                        <td>2024-09-28</td>
+                        <td>David</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+        </div>
+        <div class="pagination">
+            <a href="#">&laquo;</a>
+            <a href="#" class="active">1</a>
+            <a href="#">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>
+            <a href="#">5</a>
+            <a href="#">&raquo;</a>
+        </div>
+    </div>
+</body>
+</html>
